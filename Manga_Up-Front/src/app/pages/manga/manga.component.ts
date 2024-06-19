bc2d8c00b8af53579999818527196b2afc00fafc
SMARCOCCIA <seb@gmail.fr>
2024-06-19 19:16:51 +0200

    Ajout de mangaService et dans ngOnInt de subscribe

--- Manga_Up-Front/src/app/pages/manga/manga.component.ts
+++ Manga_Up-Front/src/app/pages/manga/manga.component.ts
@@ -1,4 +1,6 @@
+import { MangaService } from './../../services/manga.service';
 import { Component, OnInit } from '@angular/core';
+import { Manga } from '../../types';
 
 @Component({
   selector: 'app-manga',
@@ -8,26 +10,20 @@ import { Component, OnInit } from '@angular/core';
 
 <div class="flex justify-center">
   <div class="block max-w-[18rem] rounded-lg bg-secondary text-white shadow-secondary-1">
-    <div class="border-b-2 background-color-black px-6 py-3">Titre du manga</div>
+    <div class="border-b-2 background-color-black px-6 py-3">{{manga?.title}}</div>
     <div class="border-b-2 background-color-black-medium px-6 py-3">
         <div><img src="/assets/svg/star-white.svg" alt="star-white.svg"><span>12</span></div>
         <div><img src="/assets/svg/comment-white-fill.svg" alt="comment-white-fill.svg"><span>12</span></div>
         <div><img src="/assets/svg/heart-white-outline.svg" alt="heart-white-outline.svg"><span>12</span></div>
     </div>
-    <div class="border-b-2 background-color-black-light px-6 py-3"><img src="/assets/svg/star-yellow.svg" alt="">
+    <div class="border-b-2 background-color-black-light px-6 py-3"><img src="/assets/svg/star-yellow.svg" alt="star-yellow">
         <div>
             <div>-/5</div>
             <div>0 vote</div>
         </div>
     </div>
     <div class="p-6">
-      <h5 class="mb-2 text-xl font-medium leading-tight">
-        Secondary card title
-      </h5>
-      <p class="text-base">
-        Some quick example text to build on the card title and make up the
-        bulk of the card's content.
-      </p>
+        <img src="/assets/img/Lazy_Sigh_of_the_Villainous_Daughter.png" alt="Lazy_Sigh_of_the_Villainous_Daughter">
     </div>
   </div>
 </div>
@@ -38,7 +34,13 @@ import { Component, OnInit } from '@angular/core';
 
 export class MangaComponent implements OnInit{
 
+    manga!: Manga | null;
+
+    constructor(
+        private mangaService: MangaService
+    ){}
+
     ngOnInit(): void {
-        
+        this.mangaService.currentManga.subscribe(manga=>this.manga=manga);
     }
 }
