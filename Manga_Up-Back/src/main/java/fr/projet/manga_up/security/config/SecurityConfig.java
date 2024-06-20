b022be12f7bcc59774d93b74feebf78d533f0451
SMARCOCCIA <seb@gmail.fr>
2024-06-20 23:55:09 +0200

    Création du package security.config, de la classe SecurityConfig et ajout de la méthode securityFilterChain. Note doit existé pour que @CrossOrigin dans le controller puisse fonctionner et supprimer l'erreur CORS

---
+++ Manga_Up-Back/src/main/java/fr/projet/manga_up/security/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package fr.projet.manga_up.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled=true)
+public class SecurityConfig {
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		return httpSecurity
+//				.cors(cors -> cors.disable()) 
+				.authorizeHttpRequests(auth->auth.requestMatchers(
+						"/api/mangas/**",
+						"swagger-ui/**",
+						"swagger-ui.html"
+				).permitAll()).build();
+	}
+	
+}
