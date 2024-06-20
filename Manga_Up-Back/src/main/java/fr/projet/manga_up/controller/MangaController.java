2e66481f12849f5ea84f25a48c0e0c527f60a149
SMARCOCCIA <seb@gmail.fr>
2024-06-20 08:35:25 +0200

    Création de la méthode getMangaId pour la récupération d'un manga avec son id

--- Manga_Up-Back/src/main/java/fr/projet/manga_up/controller/MangaController.java
+++ Manga_Up-Back/src/main/java/fr/projet/manga_up/controller/MangaController.java
@@ -1,5 +1,31 @@
 package fr.projet.manga_up.controller;
 
-public class MangaController {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import fr.projet.manga_up.models.Manga;
+
+@RestController
+@RequestMapping("/api/mangas")
+public class MangaController {
+	
+	private static final Logger LOGGER=LoggerFactory.getLogger(MangaController.class);
+	
+	@Autowired
+	private MangaService mangaService;
+	
+	@GetMapping(value="/{id}", produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Manga> getMangaId(@PathVariable Long id){
+		LOGGER.info("Obtenir un manga");
+
+//		mangaService
+		return null;
+	}
 }
