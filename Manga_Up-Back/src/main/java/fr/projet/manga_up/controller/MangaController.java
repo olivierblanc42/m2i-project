1bc476d184ea2dd77f8662d09f398084e52b9622
SMARCOCCIA <seb@gmail.fr>
2024-06-20 23:50:24 +0200

    Ajout de @CrossOrigin pour supprimer l'erreur Cors

--- Manga_Up-Back/src/main/java/fr/projet/manga_up/controller/MangaController.java
+++ Manga_Up-Back/src/main/java/fr/projet/manga_up/controller/MangaController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import fr.projet.manga_up.models.Manga;
 import fr.projet.manga_up.services.MangaService;
 
+@CrossOrigin(origins ="*")
 @RestController
 @RequestMapping("/api/mangas")
 public class MangaController {
