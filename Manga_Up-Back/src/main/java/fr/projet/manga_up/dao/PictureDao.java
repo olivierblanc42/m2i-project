934ea4a10525dfcd2157cb72fa96efc9587c0227
SMARCOCCIA <seb@gmail.fr>
2024-06-24 07:12:50 +0200

    RAS

---
+++ Manga_Up-Back/src/main/java/fr/projet/manga_up/dao/PictureDao.java
@@ -0,0 +1,15 @@
+package fr.projet.manga_up.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import fr.projet.manga_up.model.Picture;
+
+public interface PictureDao extends CrudRepository<Picture, Integer> {
+
+	@Query(value="SELECT * FROM pictures p WHERE p.id_mangas = :id", nativeQuery=true)
+	List<Picture> findAllById(Integer id);
+
+}
