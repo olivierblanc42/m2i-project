96a628e2dd739a0d365bf456506651ddf22ce7e3
SMARCOCCIA <seb@gmail.fr>
2024-06-24 07:05:57 +0200

    Créatoin de la classe PictureService et ajout de la methode getPicturesByIdManga pour récupérer toutes les images concernant un Manga

--- Manga_Up-Back/src/main/java/fr/projet/manga_up/service/PictureService.java
+++ Manga_Up-Back/src/main/java/fr/projet/manga_up/service/PictureService.java
@@ -1,4 +1,20 @@
 package fr.projet.manga_up.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import fr.projet.manga_up.dao.PictureDao;
+import fr.projet.manga_up.model.Picture;
+
+@Component
 public class PictureService {
+	
+	@Autowired
+	private PictureDao pictureDao;
+	
+	public List<Picture> getPicturesByIdManga(Integer id) {
+		return pictureDao.findAllById(id);
+	}
 }
