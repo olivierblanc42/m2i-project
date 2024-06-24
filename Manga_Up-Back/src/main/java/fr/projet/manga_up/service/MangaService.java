069ade7b532630b3d29b303a6107938f93c7287a
SMARCOCCIA <seb@gmail.fr>
2024-06-24 07:02:56 +0200

    Suppression des import ResponseEntity et GetMapping

--- Manga_Up-Back/src/main/java/fr/projet/manga_up/service/MangaService.java
+++ Manga_Up-Back/src/main/java/fr/projet/manga_up/service/MangaService.java
@@ -5,9 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.server.ResponseStatusException;
 
 import fr.projet.manga_up.dao.MangaDao;
