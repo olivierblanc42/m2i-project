8d50adcf81635f955f5d5db505cbc8a189a33990
SMARCOCCIA <seb@gmail.fr>
2024-06-24 07:15:11 +0200

    RAS

---
+++ Manga_Up-Front/src/app/services/picture.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Picture } from '../types';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PictureService {
+    url="http://localhost:8080/api/pictures";
+
+    headers=new HttpHeaders()
+        .set("content-type", "application/json")
+        .set("Access-Control-Allow-Origin", '*');
+
+    pictures=new BehaviorSubject<Picture[]>([]);
+    currentPictures=this.pictures.asObservable();
+
+    constructor(
+        private http: HttpClient,
+    ) { }
+
+    /**
+     * Récupère les images d'un manga en fonction de son id.
+     * @param id :Id du manga
+     */
+    getPicturesByIdManga(id: string){
+        this.http.get<Picture[]>(`${this.url}/${id}`, {
+            headers: {'Access-Control-Allow-Origin': '*'}
+         })
+        .pipe()
+        .toPromise()
+        .then((r)=>{
+            if(!r) return;
+            console.log("Pictures r : ", r);
+            this.pictures.next(r);
+        })
+    }
+}
