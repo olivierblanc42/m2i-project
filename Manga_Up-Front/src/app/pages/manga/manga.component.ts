8cc9cd3755450909f4f8bc5a9f8d1200df3a4205
SMARCOCCIA <seb@gmail.fr>
2024-06-25 23:52:04 +0200

    Ajout des informations sur le manga, mise en forme, création d'une méthode strToLowerCaseAndFirstLetter pour formatter les genres

--- Manga_Up-Front/src/app/pages/manga/manga.component.ts
+++ Manga_Up-Front/src/app/pages/manga/manga.component.ts
@@ -13,24 +13,58 @@ import { ActivatedRoute } from '@angular/router';
 <div class="flex justify-center">
   <div class="block max-w-[18rem] rounded-lg bg-secondary text-white shadow-secondary-1">
     <div class="border-b-2 background-color-black px-6 py-3">{{manga?.title}}</div>
-    <div class="border-b-2 background-color-black-medium px-6 py-3">
-        <div><img src="/assets/svg/star-white.svg" alt="star-white.svg"><span>12</span></div>
-        <div><img src="/assets/svg/comment-white-fill.svg" alt="comment-white-fill.svg"><span>12</span></div>
-        <div><img src="/assets/svg/heart-white-outline.svg" alt="heart-white-outline.svg"><span>12</span></div>
+    <div class="border-b-2 background-color-black-medium px-6 py-3 flex justify-between">
+        <div class="img"><img src="/assets/svg/star-white.svg" alt="star-white.svg"><span>12</span></div>
+        <div class="img"><img src="/assets/svg/comment-white-fill.svg" alt="comment-white-fill.svg"><span>12</span></div>
+        <div class="img"><img src="/assets/svg/heart-white-outline.svg" alt="heart-white-outline.svg"><span>12</span></div>
     </div>
-    <div class="border-b-2 background-color-black-light px-6 py-3"><img src="/assets/svg/star-yellow.svg" alt="star-yellow">
-        <div>
+    <div class="border-b-2 background-color-black-light px-6 py-3 flex justify-center">
+        <div><img src="/assets/svg/star-yellow.svg" alt="star-yellow"></div>
+        <div class="ml-4">
             <div>-/5</div>
-            <div>0 vote</div>
+            <div class="text-xs">0 vote</div>
         </div>
     </div>
     <div class="h-30">
         <img class="w-72" src="{{base64}}" alt="">
     </div>
+    <div>
+        <p>Titre original : <span>{{manga?.title}}</span></p>
+        <p>Origine : </p>
+        <p>Année VF : <span>{{manga?.releaseDate}}</span></p>
+        <p>Type : <span>{{manga?.category?.name}}</span></p>
+        <p class="genres flex flex-wrap">
+            Genre : 
+            @for (genre of manga?.genres; track genre.id) {
+                <span class="ml-4">{{genre.label}}</span>
+            }
+        </p>
+        <p>Thèmes : </p>
+        <p>Auteur : </p>
+        <p>Éditeur VO : </p>
+        <p>Éditeur VF : </p>
+        <p>Prépublié dans : </p>
+        <p>Nb volume VO : 10 (Terminé)</p>
+        <p>Nb volume VF : 0 (À paraître)</p>
+        <p>Prix : <span>{{manga?.price}}</span></p>
+        <p>Âge conseillé : </p>
+        <p>Pour public averti : </p>
+        <p>Se trouve dans le commerce en France : </p>
+    </div>
   </div>
 </div>
   `,
-  styles: ``
+  styles: [`
+
+    span{
+        color: rgba(231, 224, 139, 1);
+        font-weight: inherit;
+
+    }
+
+    .genres span{
+    }
+  `]
 })
 
 export class MangaComponent implements OnInit{
@@ -54,6 +88,8 @@ export class MangaComponent implements OnInit{
         
         this.mangaService.currentManga.subscribe(manga=>{
             this.manga=manga
+            this.strToLowerCaseAndFirstLetter();
+            
         });
 
         this.pictureService.currentPictures.subscribe(pictures=>{
@@ -65,8 +101,22 @@ export class MangaComponent implements OnInit{
                 };
             }
             this.base64=this.base64+this.picture.img;
+            console.log("this.base64 : ", this.base64);
+            
         });
+    }
 
+    strToLowerCaseAndFirstLetter(){
+        if(this.manga){
+            for (let genre of this.manga?.genres) {
+                let tmp=genre.label.toLocaleLowerCase();
+                genre.label=tmp.charAt(0).toUpperCase()+tmp.slice(1);
+            }
+        }
     }
 }
+
+
+
+
 
