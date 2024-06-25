eeed04851eca98d64e31bf79b481b065fd327788
olivierblanc42 <olivierhumanbooster@gmail.com>
2024-06-25 18:55:32 +0200

    dao controller et service category

--- Manga_Up-Back/src/main/java/fr/projet/manga_up/security/config/SecurityConfig.java
+++ Manga_Up-Back/src/main/java/fr/projet/manga_up/security/config/SecurityConfig.java
@@ -4,12 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import fr.projet.manga_up.controller.MangaController;
 
@@ -28,6 +25,7 @@ public class SecurityConfig {
 				"/api/pictures/**",
 	        	"/api/mangas/**",
 	        	"/api/genres/**",
+						"/api/categories/**",
 	        	"/swagger-ui/**", 
 	        	"/v3/api-docs/**", 
 	        	"/swagger-ui.html", 
