96e6db1ff695e70b856e6a7398c6909eea56f17c
olivierblanc42 <olivierhumanbooster@gmail.com>
2024-06-26 19:21:10 +0200

    modification footer responsive

--- Manga_Up-Front/src/app/app.component.ts
+++ Manga_Up-Front/src/app/app.component.ts
@@ -9,8 +9,8 @@ import { faBook, faSearch, faUser, faCartShopping, faShuffle } from '@fortawesom
   imports: [RouterOutlet, RouterModule,FontAwesomeModule],
   template: `
 
-
-     <!-- nav mobile -->
+<div class="container mx-auto">
+       <!-- nav mobile -->
    <nav class="flex   py-3 justify-evenly nav-mobile" >
    <ul class="flex flex-row items-center gap-x-8 ">
     <li><a routerLink="/"><img  src="assets/img/logo.png" alt=""></a></li>
@@ -47,20 +47,31 @@ import { faBook, faSearch, faUser, faCartShopping, faShuffle } from '@fortawesom
     </div>
 </nav>
 
+</div>
+
+
+
+
     <router-outlet/>
 
 
          <!-- Footer mobile -->
 
-     <footer class="bg-dark-up py-3 text-white footer-mobile">
-        <img class="mx-auto"  src="assets/img/logo.png" alt="">
+     <footer class="bg-dark-up py-3 text-white" >
+       <div class=" flex flex-col md:flex-row md:px-10 justify-around items-center	" >
 
-        <div class="text-center my-6">
-            <img class="mx-auto" src="assets/img/stars.png" alt=""> 
+ 
+    <div class=" flex flex-col w-1/3  "   >
+         <img class=" mx-auto w-40"  src="assets/img/logo.png" alt="">
+
+        <div class="my-6 mx-auto	">
+            <img class="" src="assets/img/stars.png" alt="">
             <p class="infos">Score<span> 4.1</span> | <span>1500</span> avis</p>
         </div>
+     </div>
 
-        <div class="text-center my-6"  >
+<div class=" flex flex-col md:flex-row w-1/3 justify-between"   >
+ <div class="text-center my-6 md:text-start"   >
             <ul class="list" >
                 <li><h4>Manga’up</h4></li>
                 <li><a>Les conditions de vente</a></li>
@@ -69,83 +80,34 @@ import { faBook, faSearch, faUser, faCartShopping, faShuffle } from '@fortawesom
             </ul>
         </div>
 
-        <div class="text-center my-6"  >
+        <div class="text-center md:text-start my-6"  >
             <ul class="list">
                 <li><h4>Compte</h4></li>
                 <li><a>Créer un compte</a></li>
                 <li><a>Se connecter</a></li>
             </ul>
         </div>
-        <div class="flex flex-row justify-center gap-4 my-6">
-            <a href="https://discord.com/" target="_blank"><img src="assets/icon/discord_mobile.png" alt=""></a> 
-            <a href="https://x.com/home" target="_blank"><img src="assets/icon/x_mobile.png" alt=""></a> 
-            <a href="https://www.instagram.com/" target="_blank"><img src="assets/icon/insta_mobile.png" alt=""></a>
-            <a href="https://www.facebook.com/" target="_blank"><img src="assets/icon/facebook_mobile.png" alt=""></a>
-        </div>
+</div>
 
-     <img class="mx-auto my-6" src="assets/img/android-btn.png" alt=""> 
-
-        <img class="mx-auto my-6"  src="assets/img/line.png" alt="">
-
-        <p class="text-center "  >Copyright © 2024 Manga’up
-       <br>Contact, Mention legale - All rights reserved</p>
-    </footer>
-
-     <!-- Footer desktop -->
-
-  <footer class="background-color py-3 flex flex-col px-10 footer-desktop ">
-
-<div class="flex flex-row 	justify-between	"   >
-    <div class=" flex flex-col	 justify-between 	">
-        <div >
-            <img class="mx-auto" src="assets/img/stars.png" alt="">
-            <p class="infos">Score<span> 4.1</span> | <span>1500</span> avis</p>
-        </div>
-        <img class=" inline-block "  src="assets/img/logo.png" alt="">
-    </div>
-
-
-    <div>
-     <ul class="list" >
-                <li><h4>Manga’up</h4></li>
-                <li><a>Les conditions de vente</a></li>
-                <li><a>Politique de confidentialité</a></li>
-                <li><a>contact</a></li>
-            </ul>
-    </div>
- 
-
-
-        <div class=" "  >
-            <ul class="list">
-                <li><h4>Compte</h4></li>
-                <li><a>Créer un compte</a></li>
-                <li><a>Se connecter</a></li>
-            </ul>
-        </div>
-
-      <div class=" flex flex-col	 justify-between ">
-
-        <div class=" flex flex-row justify-center gap-4 ">
+<div  class=" flex flex-col md:flex-col w-1/3  ">
+        <div class="flex flex-row mx-auto gap-4 my-6 ">
             <a href="https://discord.com/" target="_blank"><img src="assets/icon/discord_mobile.png" alt=""></a>
             <a href="https://x.com/home" target="_blank"><img src="assets/icon/x_mobile.png" alt=""></a>
             <a href="https://www.instagram.com/" target="_blank"><img src="assets/icon/insta_mobile.png" alt=""></a>
             <a href="https://www.facebook.com/" target="_blank"><img src="assets/icon/facebook_mobile.png" alt=""></a>
         </div>
-             <img class="ml-auto " src="assets/img/android-btn.png" alt="">
-
-      </div>
-
+             <img class="mx-auto " src="assets/img/android-btn.png" alt="">
 </div>
-
-        <img class="mx-auto my-6"  src="assets/img/line_desktop.png" alt="">
-
-
-      <p class=" ">Copyright © 2024 Manga’up Contact, Mention legale - All rights reserved</p>
-        
-  </footer>
+       </div> 
+ 
 
 
+        <img class="my-6 		 mx-auto   w-8/12 "  src="assets/img/line_desktop.png" alt="">
+
+        <p class="text-center "  >Copyright © 2024 Manga’up
+       <br>Contact, Mention legale - All rights reserved</p>
+
+    </footer>
 
   `,
   styles: [`
