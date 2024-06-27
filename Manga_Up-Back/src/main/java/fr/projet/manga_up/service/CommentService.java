99f731bad80123ac21316c3472f032cd4caeb2c0
SMARCOCCIA <seb@gmail.fr>
2024-06-27 11:07:20 +0200

    Ajout de la méthode de récupération des commentaire par l'id du manga

--- Manga_Up-Back/src/main/java/fr/projet/manga_up/service/CommentService.java
+++ Manga_Up-Back/src/main/java/fr/projet/manga_up/service/CommentService.java
@@ -1,4 +1,20 @@
 package fr.projet.manga_up.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import fr.projet.manga_up.dao.CommentDao;
+import fr.projet.manga_up.model.Comment;
+
+@Component
 public class CommentService {
+	
+	@Autowired
+	private CommentDao commentDao;
+	
+	public List<Comment> getCommentsByIdManga(Integer id) {
+		return commentDao.findAllById(id);
+	}
 }
