014a3116a1db1c4ffacce33fbbc3d00bd5086a41
olivierblanc42 <olivierhumanbooster@gmail.com>
2024-06-27 10:57:58 +0200

    ajout @repository dao genre et category

--- Manga_Up-Back/src/main/java/fr/projet/manga_up/dao/CategoryDao.java
+++ Manga_Up-Back/src/main/java/fr/projet/manga_up/dao/CategoryDao.java
@@ -1,9 +1,11 @@
 package fr.projet.manga_up.dao;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import fr.projet.manga_up.model.Category;
 
+@Repository
 public interface CategoryDao extends CrudRepository<Category, Integer> {
 
 }
--- Manga_Up-Back/src/main/java/fr/projet/manga_up/dao/GenreDao.java
+++ Manga_Up-Back/src/main/java/fr/projet/manga_up/dao/GenreDao.java
@@ -1,9 +1,11 @@
 package fr.projet.manga_up.dao;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import fr.projet.manga_up.model.Genre;
 
+@Repository
 public interface GenreDao extends CrudRepository<Genre, Integer> {
 
 }
