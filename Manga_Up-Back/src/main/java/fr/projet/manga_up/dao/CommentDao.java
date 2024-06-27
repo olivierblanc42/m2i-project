5dc0ef68148a4938d6b1fbcd182e88b3ea47139b
SMARCOCCIA <seb@gmail.fr>
2024-06-27 08:47:38 +0200

    Crétion du dao CommentDao

---
+++ Manga_Up-Back/src/main/java/fr/projet/manga_up/dao/CommentDao.java
@@ -0,0 +1,13 @@
+package fr.projet.manga_up.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import fr.projet.manga_up.model.Comment;
+
+public interface CommentDao extends CrudRepository<Comment, Integer> {
+	@Query(value="SELECT * FROM comments p WHERE p.id_mangas = :id", nativeQuery=true)
+	List<Comment> findAllById(Integer id);
+}
