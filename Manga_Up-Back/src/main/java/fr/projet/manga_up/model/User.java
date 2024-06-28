b70f225cec93a7d43c0465d206fd3c7796986c0c
SMARCOCCIA <seb@gmail.fr>
2024-06-28 08:20:50 +0200

    Suppression de l'attribut uniqueConstrainte, ajout de l'annotation JsonIgnor sur les attributs address et gender, ajout d'une relation oneToMany sur l'attribut comments et ses getter/setter

--- Manga_Up-Back/src/main/java/fr/projet/manga_up/model/User.java
+++ Manga_Up-Back/src/main/java/fr/projet/manga_up/model/User.java
@@ -1,13 +1,27 @@
 package fr.projet.manga_up.model;
 
-import jakarta.persistence.*;
-
 import java.time.Instant;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "users", schema = "manga_up", uniqueConstraints = {
-        @UniqueConstraint(name = "users_AK", columnNames = {"email"})
-})
+@Table(name = "users", schema = "manga_up")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -38,11 +52,17 @@ public class User {
     
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "Id_addresses", nullable = false)
+    @JsonIgnore
     private Address address;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "Id_genders", nullable = false)
+    @JsonIgnore
     private Gender gender;
+    
+    @OneToMany(mappedBy="user")
+    @JsonManagedReference
+    private List<Comment> comments;
 
     public Integer getId() {
         return id;
@@ -124,5 +144,14 @@ public class User {
 		this.gender = gender;
 	}
 
+	public List<Comment> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<Comment> comments) {
+		this.comments = comments;
+	}
+
+	
 
 }
