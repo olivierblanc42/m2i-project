19bdd251409a866407f0fe153962d3a37610be8d
Nasser <nasserbrid@gmail.com>
2024-06-28 13:49:57 +0200

    modifs

--- Manga_Up-Back/src/main/java/fr/projet/manga_up/controller/MangaController.java
+++ Manga_Up-Back/src/main/java/fr/projet/manga_up/controller/MangaController.java
@@ -1,5 +1,7 @@
 package fr.projet.manga_up.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,34 +17,34 @@ import fr.projet.manga_up.model.Manga;
 import fr.projet.manga_up.service.MangaService;
 
 
-@CrossOrigin(origins = "*") 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/mangas")
 public class MangaController {
 	private static final Logger LOGGER=LoggerFactory.getLogger(MangaController.class);
-	
+
 	@Autowired
 	private MangaService mangaService;
-	
+
 	/**
-	 * 
+	 *
 	 * @param id L'id qui représente le Manga que l'on souhaite obtenir.
 	 * @return Retourne le Manga de l'id spécifié.
 	 */
 	@GetMapping(value="/{id}", produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Manga> getManga(@PathVariable Integer id){
-//		LOGGER.info("Méthode getMangaId, id : {}", id);
+		LOGGER.info("Méthode getMangaId, id : {}", id);
 		Manga manga=mangaService.getManga(id);
-//		LOGGER.info("Manga : {}", manga);
+		LOGGER.info("Manga : {}", manga);
 		return ResponseEntity.ok(manga);
 	}
 
 	@GetMapping()
-	public ResponseEntity<?> getTenManga() {
+	public ResponseEntity<List<Manga>> getTenManga() {
 		LOGGER.info("Récupération de 10 manga");
-		Manga manga = (Manga) mangaService.getTenManga();
-		LOGGER.info("Manga : {}", manga);
-		return ResponseEntity.ok(manga);
+		List<Manga> mangas =  mangaService.getTenManga();
+		LOGGER.info("Mangas : {}", mangas);
+		return ResponseEntity.ok(mangas);
 	}
 
 }
